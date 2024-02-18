442d507c0143ebed9e9bac1f774568fb3060a6da
Lachlan Holland <lachholland@gmail.com>
2024-02-18 14:50:54 +1000

    modify sports database queries

--- sports/db/db.go
+++ sports/db/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (r *sportEventsRepo) seed() error {
-	statement, err := r.db.Prepare(`CREATE TABLE IF NOT EXISTS sportEvents (id INTEGER PRIMARY KEY, meeting_id INTEGER, name TEXT, number INTEGER, visible INTEGER, advertised_start_time DATETIME)`)
+	statement, err := r.db.Prepare(`CREATE TABLE IF NOT EXISTS sportEvents (id INTEGER PRIMARY KEY, name TEXT, advertised_start_time DATETIME)`)
 	if err == nil {
 		_, err = statement.Exec()
 	}
@@ -17,10 +17,7 @@ func (r *sportEventsRepo) seed() error {
 		if err == nil {
 			_, err = statement.Exec(
 				i,
-				faker.Number().Between(1, 10),
 				faker.Team().Name(),
-				faker.Number().Between(1, 12),
-				faker.Number().Between(0, 1),
 				faker.Time().Between(time.Now().AddDate(0, 0, -1), time.Now().AddDate(0, 0, 2)).Format(time.RFC3339),
 			)
 		}
